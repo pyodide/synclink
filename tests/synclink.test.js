679f812aade2a4e32dae3727aa81de1f573c37ae
Hood <hood@mit.edu>
2021-07-24 09:09:18 -0700

    Add some untracked files

---
+++ tests/synclink.test.js
@@ -0,0 +1,72 @@
+import * as Comlink from "/base/dist/esm/comlink.mjs";
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+describe("xxx xxxx xxxxx", function () {
+  beforeEach(async function () {
+    this.worker = Comlink.wrap(
+      new Worker("/base/tests/synclink.test.worker.js")
+    );
+    const fetch = window.fetch.bind(window);
+    this.worker_scope = { fetch };
+    await this.worker.set_global_scope(Comlink.proxy(this.worker_scope));
+  });
+
+  afterEach(function () {
+    this.worker.terminate();
+  });
+
+  it("blah", async function () {
+    const ttt = 999;
+    this.worker_scope.ttt = ttt;
+    expect(await this.worker.get_from_main_window("ttt")).to.equal(ttt);
+    expect(await this.worker.test("debug.html")).to.contain(
+      "This file is almost the same as context.html"
+    );
+    expect(await this.worker.test2("debug.html")).to.contain(
+      "This file is almost the same as context.html"
+    );
+  });
+
+  it("blah3", async function () {
+    expect(await this.worker.test3("debug.html")).to.equal(
+      JSON.stringify({
+        type: "basic",
+        redirected: false,
+        status: 200,
+        ok: true,
+        statusText: "OK",
+      })
+    );
+  });
+
+  it("blah4", async function () {
+    this.x = 0;
+    const delay = 10;
+    let f = async () => {
+      await sleep(delay);
+      sleep(delay).then(() => this.x++);
+      return this.x;
+    };
+    this.worker_scope.f = f;
+    expect(await this.worker.test4("f")).to.equal(
+      JSON.stringify([0, 0, 0, 0, 0])
+    );
+  });
+
+  it("blah5", async function () {
+    this.x = 0;
+    const delay = 10;
+    let f = async () => {
+      await sleep(delay);
+      sleep(delay).then(() => this.x++);
+      return this.x;
+    };
+    this.worker_scope.f = f;
+    expect(await this.worker.test5("f")).to.equal(
+      JSON.stringify([0, 1, 2, 3, 4])
+    );
+  });
+});
---
+++ tests/synclink.test.worker.js
@@ -0,0 +1,64 @@
+importScripts("/base/dist/umd/comlink.js");
+async function get_from_main_window(x) {
+  return await self.main_window[x];
+}
+
+async function test(x) {
+  let resp = await self.main_window.fetch(x);
+  console.log(resp);
+  let value = await resp.text();
+  return value;
+}
+
+function test2(x) {
+  let resp = self.main_window.fetch(x).syncify();
+  let value = resp.text().syncify();
+  return value;
+}
+function test3(x) {
+  const resp = self.main_window.fetch(x).syncify();
+  const scheduled = {};
+  const keys = ["type", "redirected", "status", "ok", "statusText"];
+  for (let key of keys) {
+    scheduled[key] = resp[key].schedule_sync();
+  }
+  const results = {};
+  for (let key of keys) {
+    results[key] = scheduled[key].syncify();
+  }
+  return JSON.stringify(results);
+}
+
+function test4(name) {
+  const tasks = [];
+  for (let i = 0; i < 5; i++) {
+    tasks.push(self.main_window[name]().schedule_sync());
+  }
+  const results = [];
+  for (let task of tasks) {
+    results.push(task.syncify());
+  }
+  return JSON.stringify(results);
+}
+
+function test5(name) {
+  const results = [];
+  for (let i = 0; i < 5; i++) {
+    results.push(self.main_window[name]().syncify());
+  }
+  return JSON.stringify(results);
+}
+
+function set_global_scope(window) {
+  self.main_window = window;
+}
+
+Comlink.expose({
+  get_from_main_window,
+  set_global_scope,
+  test,
+  test2,
+  test3,
+  test4,
+  test5,
+});
