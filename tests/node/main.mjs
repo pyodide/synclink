fc1ba8e7f833730e58686f544e8c191f3de67bc2
Eduardo Rodrigues <16357187+eduardomourar@users.noreply.github.com>
2023-09-19 17:29:34 +0200

    chore: return promise in nodejs test

--- tests/node/main.mjs
+++ tests/node/main.mjs
@@ -1,4 +1,4 @@
-import { Worker } from "node:worker_threads";
+import { Worker, MessageChannel } from "node:worker_threads";
 import * as Synclink from "../../dist/esm/synclink.mjs";
 import nodeEndpoint from "../../dist/esm/node-adapter.mjs";
 import { expect } from "chai";
@@ -19,9 +19,30 @@ describe("node", () => {
       expect(await proxy(1, 3)).to.equal(4);
     });
 
-    it("can communicate synchronously", function () {
+    it("can work with objects + syncify in same thread", function () {
+      let fakePort1, fakePort2; 
+      const { port1, port2 } = new MessageChannel();
+      ({ port1: fakePort1, port2: fakePort2 } =
+        new Synclink.FakeMessageChannel());
+      port1.start();
+      port2.start();
+      const thing = Synclink.wrap(fakePort1);
+      Synclink.expose(
+        {
+          value: 4,
+          func(n) {
+            return n + 10;
+          },
+        },
+        fakePort2,
+      );
+      expect(thing.value.syncify()).to.equal(4);
+      expect(thing.func(5).syncify()).to.equal(15);
+    });
+
+    it("can communicate synchronously", async function () {
       const proxy = Synclink.wrap(nodeEndpoint(this.worker));
-      expect(proxy(1, 3).syncify()).to.equal(4);
+      expect(proxy(2, 4).syncify()).to.equal(6);
     });
 
     it("can tunnels a new endpoint with createEndpoint", async function () {
